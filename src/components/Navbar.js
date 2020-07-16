92b00545b80822d43691e685097757555e3ee263
Vaibhav Verma <56814076+vverma2010@users.noreply.github.com>
2020-07-16 14:08:39 +0530

    Improved UI

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ class Navbar extends React.Component {
     return (
         <div className = "nav">
           <div className ="search-container">
-              <input/>
+              <input placeholder="Search movies..."/>
               <button id="search-btn">Search</button>
           </div>
       </div>
